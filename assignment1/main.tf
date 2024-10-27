9a0ec8481fc229733c485d4483df21a190b800d9
123arit <39845239+123arit@users.noreply.github.com>
2024-10-28 00:52:32 +0530

    Update main.tf

--- assignment1/main.tf
+++ assignment1/main.tf
@@ -1,3 +1,5 @@
+#### Create a security group in aws cloud ####
+
 terraform {
   required_providers {
     aws = {
@@ -10,14 +12,36 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
+  access_key = "my-access-key" #this is a very bad way to authenticate, only used here for demo purpose
+  secret_key = "my-secret-key" #this is a very bad way to authenticate, only used here for demo purpose
 }
 
 provider "aws" {
-  alias = "euwest"
-  region = "eu-west-2" # London
+  alias = "mumbai"
+  region = "ap-south-1" # Mumbai
+  access_key = "my-access-key" #this is a very bad way to authenticate, only used here for demo purpose
+  secret_key = "my-secret-key" #this is a very bad way to authenticate, only used here for demo purpose
 }
 
-resource "time_sleep" "wait_30_seconds" {
+resource "aws_security_group" "example" {
+  name        = "terraform_lab_allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  provider = aws.mumbai
 
-  create_duration = "30s"
+  ingress {
+      description      = "HTTPS from VPC"
+      from_port        = 443
+      to_port          = 443
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 }
